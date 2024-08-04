b4d11e7462898d4b90d14f68e969c3b4f79096e9
Guilherme Brizzi <brizzigui@gmail.com>
2024-08-03 22:14:51 -0300

    polynomial interpolation

--- q2.py
+++ q2.py
@@ -78,12 +78,49 @@ def polynomial_printing(p:list) -> None:
         else:
             print(f"{value}x^{index}", end=" ") if value != 1 else print(f"x^{index}", end=" ")
 
+def get_applied_polynomial(p: list, value: float) -> float:
+    acc = 0
+    for i in range(len(p)):
+        acc = acc + (p[i]*value**i)
+
+    return acc
+
+def divide_polynomial_by_value(polynomial: list, value: float) -> list:
+    return [polynomial[i]/value for i in range(len(polynomial))]
+
+def convert_timestamp(stamp: str) -> float:
+    # Retorna o tempo em horas decorrido desde o início do experimento.
+    pass
 
 def plot_interpolated_function(p:list) -> None:
+    # plotar função polinomial com matplot e pontos discretos. x sendo as horas, y os valores
     pass
 
-def interpolate_data_points() -> None:
-    pass
+def interpolate_data_points(x_points: list, y_points: list) -> None:
+    # Usando o algoritmo de lagrange de interpolação polinomial
+
+    size = len(x_points)
+    polynomial = [0] * size
+    
+    for j in range(size):
+        first = True
+        for i in range(size):
+            if i == j:
+                continue
+
+            if first:
+                first = False
+                aux = [-x_points[i], 1]
+                aux = divide_polynomial_by_value(aux, x_points[j] - x_points[i])
+           
+            aux = polynomial_multiplication(aux, [-x_points[i], 1])
+            aux = divide_polynomial_by_value(aux, x_points[j] - x_points[i])
+
+        aux = polynomial_multiplication([y_points[j]], aux)
+        polynomial = polynomial_addition(polynomial, aux)
+
+    return polynomial
+        
 
 def main() -> None:
     with open("./dados_ic.csv", "r", encoding="utf8") as file:
@@ -99,8 +136,9 @@ def main() -> None:
 
         # plot_wvht(dates, wvht)
 
-        result = polynomial_multiplication([-12, 1, 3], [4, 0, 1])
+        result = interpolate_data_points([0, 1, 2, 3, 5], [4.13, 3.01, 2.79, 3.94, 2.7])
         polynomial_printing(result)
+        print(get_applied_polynomial(result, 3))
 
 
 # Para evitar confusão com escopo de variáveis
